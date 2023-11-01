89fcac037d55a7ae9ca59ac8b5d9e827ace4de81
Hofstetter Reto <reto.hofstetter@unilu.ch>
2023-11-01 13:36:29 +0100

    upload

---
+++ r_copilot_examples.R
@@ -0,0 +1,15 @@
+
+# calculate the average fuel efficiency of cars in the mtcars dataset
+# that have more than 100 horsepower
+
+library(dplyr)
+
+mtcars %>%
+  filter(hp > 100) %>%
+  summarise(mean(mpg))
+
+# calculate the average fuel efficiency of cars in the mtcars dataset grouped by the number of cylinders
+
+mtcars %>%
+  group_by(cyl) %>%
+  summarise(mean(mpg))
